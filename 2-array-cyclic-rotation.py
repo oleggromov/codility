c44abbbacb3f3d1f5fd7d445b22bdd67905bd89f
Oleg Gromov <mail@oleggromov.com>
2018-03-17 18:08:52 +0100

    Adds solution employing list comprehensions and concatenation

--- 2-array-cyclic-rotation.py
+++ 2-array-cyclic-rotation.py
@@ -1,4 +1,4 @@
-def solution(arr, k):
+def solution1(arr, k):
   result = []
   length = len(arr)
 
@@ -10,7 +10,7 @@ def solution(arr, k):
 
   return result
 
-def solution_simplified(arr, k):
+def solution2(arr, k):
   result = []
   length = len(arr)
 
@@ -22,10 +22,17 @@ def solution_simplified(arr, k):
 
   return result
 
-assert solution_simplified([], 3) == []
-assert solution_simplified([1,2,3], 1) == [3,1,2]
-assert solution_simplified([4,5,6], 2) == [5,6,4]
-assert solution_simplified([7,8,9], 3) == [7,8,9]
-assert solution_simplified([8,9,0], 4) == [0,8,9]
-assert solution_simplified([3,8,9,7,6], 3) == [9,7,6,3,8]
+def solution3(arr, k):
+  len_arr = len(arr)
+  split_point = len_arr - k % len_arr if len_arr > 0 else 1
+  return arr[split_point:] + arr[0:split_point]
+
+assert solution3([], 3) == []
+assert solution3([1,2,3], 1) == [3,1,2]
+assert solution3([4,5,6], 2) == [5,6,4]
+assert solution3([7,8,9], 3) == [7,8,9]
+assert solution3([8,9,0], 4) == [0,8,9]
+assert solution3([1,2,3,4,5], 9) == [2,3,4,5,1]
+assert solution3([1,1,2,3,5], 42) == [3,5,1,1,2] # codility failed test
+assert solution3([3,8,9,7,6], 3) == [9,7,6,3,8]
 
